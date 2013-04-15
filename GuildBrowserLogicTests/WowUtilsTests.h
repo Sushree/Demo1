1c64a2794fd3f14d193eda1bb148ef4fbeba32ba
Sushree <Shahid@adea1.adeasolutions.com>
2013-04-15 18:57:42 +0530

    Unit test Commit

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,12 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Sushree on 15/04/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTests.m
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.m
+//  GuildBrowser
+//
+//  Created by Sushree on 15/04/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import "WowUtilsTests.h"
+
+@implementation WowUtilsTests
+
+@end
